6f28055f132e262e09d6727119a6f7c7909b0168
Eldar Sandanger <Eldar.sandanger@gmail.com>
2022-09-27 14:36:06 +0200

    Generating several waypoints working

---
+++ Assets/Scenarios/Waypoint finder/Matlab/generateWaypoints.m
@@ -0,0 +1,10 @@
+function [waypoint] = generateWaypoints(startPoint, endPoint);
+
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+    waypoint = ( startPoint + endPoint ) / 2;
+
+end
+
--- Assets/Scenarios/Waypoint finder/Systems/WaypointGenerationSystem.cs
+++ Assets/Scenarios/Waypoint finder/Systems/WaypointGenerationSystem.cs
@@ -9,46 +9,55 @@ using UnityEngine;
 public partial class WaypointGenerationSystem : SystemBase
 {
     private Entity _waypointEntity;
-    
-    // The following will have to be fetched from each universe individually in concept
-    // private Entity _waypointSpawner?
-    // private Random _random;
-    // private float3 _minPos = float3.zero;
-    // private float3 _maxPos = new float3(50, 0, 50);
+    //private MLApp.MLApp _matlab;
 
     private EndInitializationEntityCommandBufferSystem _ecbSystem;
 
     protected override void OnStartRunning()
     {
-        //_waypointPrefab = GetSingletonEntity<WaypointPrefab>();
-
-
+        //_matlab = new MLApp.MLApp();
         _ecbSystem = World.GetOrCreateSystem<EndInitializationEntityCommandBufferSystem>();
-                // Setup EntityManager and make an entity instance of our universe prefab
-
-
-        // May be useful?
-        //_capsuleSpawner = GetSingletonEntity<LastSpawnedCapsule>();
-        //EntityManager.AddBuffer<SpawnedCapsuleBufferElement>(_capsuleSpawner);
     }
 
     protected override void OnUpdate()
     {
         var ecb = _ecbSystem.CreateCommandBuffer().AsParallelWriter();        
-        //var spawnerQuery = EntityManager.CreateEntityQuery(typeof(SpawnWaypointsTag), ComponentType.ReadOnly<WaypointGenerationData>()); // Basically ask "give me every entity that have this specific component!"
         
         Entities
             .WithAll<SpawnWaypointsTag>()
             .ForEach((Entity e, int entityInQueryIndex, in WaypointGenerationData data, in AreaDefinition area, in Translation translation) => {
 
-                var newWaypoint = ecb.Instantiate(entityInQueryIndex, data.WaypointPrefab);
-                var newPos = new Translation { Value = new float3(20, 0, 20) + translation.Value};
-                ecb.SetComponent(entityInQueryIndex, newWaypoint, newPos);
+                var numberOfWaypoints = data.Count;
+                var startPoint = data.StartPoint;
+                var endPoint = data.EndPoint;
+                Debug.Log("Entity: " + entityInQueryIndex.ToString());
+                float3[] waypoints = calculateWaypoints(startPoint, endPoint, numberOfWaypoints);
+
+                foreach(float3 point in waypoints){
+                    var newWaypoint = ecb.Instantiate(entityInQueryIndex, data.WaypointPrefab);
+                    var newPos = new Translation { Value = point + translation.Value};
+                    ecb.SetComponent(entityInQueryIndex, newWaypoint, newPos);
+                }
 
                 // We have spawned waypoints now. Remove component..
                 ecb.RemoveComponent<SpawnWaypointsTag>(entityInQueryIndex, e);
 
         }).WithoutBurst().Run();
         _ecbSystem.AddJobHandleForProducer(this.Dependency);
+    }
+
+    float3[] calculateWaypoints(float3 startPoint, float3 endPoint, int count){
+
+        float3[] list = new float3[count];
+        float3 spacing = math.abs(( endPoint - startPoint ) / count);
+        Debug.Log(spacing.ToString());
+        Debug.Log(count.ToString());
+
+        for(int i = 0; i < count; i++){
+            list[i] = startPoint + spacing * i;
+            Debug.Log(list[i].ToString());
+        }
+
+        return list;
     }
 }
